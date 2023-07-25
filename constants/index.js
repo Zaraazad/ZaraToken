3b8901a662bd6ea5d59f4b054cc3df0ade203205
Zara Az <94697744+Zaraazad@users.noreply.github.com>
2023-07-25 14:55:06 +0330

    Update index.js
    for updating my contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0e0b93b62074fa6dea8ec0d838d60902ff77a006";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
